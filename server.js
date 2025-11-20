f7c69a2c218a1a98c0cbcb31294b1c33860665f3
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-20 11:25:14 +0530

    Stored the user data in the obj and retrived it

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express()
 const port = 8080
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
+
+let user_obj = {}
 
 app.post("/signup", (req, res) => {
     const sign_up_obj = req.body
     let valid = sign_up.validate(sign_up_obj)
     if(valid.error) res.json({success : false, message : valid.error.message})
+    user_obj = req.body
     res.json({success : true, message : "sign-up completed"})
 })
 
@@ -19,11 +22,15 @@ app.post("/login", (req, res) => {
     const login_obj = req.body
     let valid = login.validate(login_obj)
     if(valid.error) res.json({success : false, message : valid.error.message})
-    res.json({success : true, message : "Login Successfull"})
+    if(req.body.email_id == user_obj.email_id && req.body.pass == user_obj.pass)
+    {
+        res.json({User_Details : user_obj, message : "Login Successful"})
+    }
+    res.json({success : false, message : "Invalid email or password"})
 })
 
 app.get("/", (req,res) => { 
-    res.send("<h>Hello World!</h>")
+    res.send("<h>Api is working!</h>")
 })
 
 app.listen(port, () => { console.log(`Server listening on port ${port}`)})
