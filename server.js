b62baf96774fb2765ec1e502d4a7b443182dacac
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-20 16:39:18 +0530

    Implemented jwt and stored in the cookies

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import {sign_up, login} from './joi.js'
 import express from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv"
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
 
 const app = express()
 
@@ -10,6 +15,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 let User_Map = new Map()
+let User_id = 1
+
+app.use(cookieParser())
 
 app.post("/signup", async (req, res) => {
 
@@ -20,6 +28,7 @@ app.post("/signup", async (req, res) => {
     let hashpass = await bcrypt.hash(req.body.pass, 10)
 
     let details = {
+        id : User_id++,
         name : req.body.name,
         age : req.body.age,
         email_id : req.body.email_id,
@@ -27,6 +36,15 @@ app.post("/signup", async (req, res) => {
     }
 
     User_Map.set(req.body.email_id, details)
+
+    const token = jwt.sign({key : req.body.email_id}, process.env.JWT_SIGN)
+
+    res.cookie('token', token, {
+        httpOnly : true,
+        secure : true, //true -> https
+        sameSite : false, // to prevent CSRF
+        maxAge : 24 * 60 * 60 * 1000
+    })
 
     res.json({success : true, message : "sign-up completed"})
 })
@@ -46,7 +64,16 @@ app.post("/login", async (req, res) => {
         const isMatch = await bcrypt.compare(req.body.pass, obj.pass)
         if(isMatch)
         {
-            res.json({name : obj.name, age : obj.age, email_id : obj.email_id, pass : req.body.pass})
+            const token = jwt.sign({key : req.body.email_id}, process.env.JWT_SIGN)
+
+            res.cookie('token', token, {
+                httpOnly : true,
+                secure : true, //true -> https
+                sameSite : false, // to prevent CSRF
+                maxAge : 24 * 60 * 60 * 1000
+            })
+            
+            res.json({success : true, message : "Login Successfull"})
         }
         else
         {
@@ -57,6 +84,25 @@ app.post("/login", async (req, res) => {
     {
         res.json({success : false, message : "No data is available"})
     }
+})
+
+app.get("/getdetails", async (req, res) => {
+    
+    let { token } = req.cookies
+
+    const isvalid = jwt.verify(token, process.env.JWT_SIGN)
+
+    if(isvalid.key)
+    {
+        let obj = User_Map.get(isvalid.key)
+
+        res.json({id : obj.id, name : obj.name, age : obj.age, email_id : obj.email_id})
+    }
+    else
+    {
+        res.json({success : false, message : "Invalid token or expired"})
+    }
+
 })
 
 app.get("/", (req,res) => { 
