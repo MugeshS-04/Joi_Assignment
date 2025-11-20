db0971d90e88f5dd5c4c78e76ca6afa03fa676cd
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-20 12:12:44 +0530

    Implemented password hashing using bcrypt and hashmap for faster checkups for login

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
 import {sign_up, login} from './joi.js'
 import express from 'express'
+import bcrypt from 'bcrypt'
 
 const app = express()
 
 const port = 8080
 
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-let user_obj = {}
+let User_Map = new Map()
 
-app.post("/signup", (req, res) => {
-    const sign_up_obj = req.body
-    let valid = sign_up.validate(sign_up_obj)
+app.post("/signup", async (req, res) => {
+
+    let valid = sign_up.validate(req.body)
+
     if(valid.error) res.json({success : false, message : valid.error.message})
-    user_obj = req.body
+    
+    let hashpass = await bcrypt.hash(req.body.pass, 10)
+
+    let details = {
+        name : req.body.name,
+        age : req.body.age,
+        email_id : req.body.email_id,
+        pass : hashpass
+    }
+
+    User_Map.set(req.body.email_id, details)
+
     res.json({success : true, message : "sign-up completed"})
 })
 
-app.post("/login", (req, res) => {
-    const login_obj = req.body
-    let valid = login.validate(login_obj)
+app.post("/login", async (req, res) => {
+
+    let valid = login.validate(res.body)
+
     if(valid.error) res.json({success : false, message : valid.error.message})
-    if(req.body.email_id == user_obj.email_id && req.body.pass == user_obj.pass)
-    {
-        res.json({User_Details : user_obj, message : "Login Successful"})
+        
+    let key = req.body.email_id
+        
+    if(User_Map.has(key))
+    {    
+        let obj = User_Map.get(key)
+        
+        const isMatch = await bcrypt.compare(req.body.pass, obj.pass)
+        if(isMatch)
+        {
+            res.json({name : obj.name, age : obj.age, email_id : obj.email_id, pass : req.body.pass})
+        }
+        else
+        {
+            res.json({message : "The password is incorrect"})
+        }
     }
-    res.json({success : false, message : "Invalid email or password"})
+    else 
+    {
+        res.json({success : false, message : "No data is available"})
+    }
 })
 
 app.get("/", (req,res) => { 
